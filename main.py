176d91f093a0ec6ecddb7756ba21daf3006c9b9a
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-16 13:53:41 +0100

    fix: cors error

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from database import supabase
 from models import ActivitySwipe
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/activities")
 def read_activities():
