a64ab1187845cce01b73b0013acbc0f4e163c874
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-21 23:16:39 +0100

    generate embedings

---
+++ embedings.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
+
+api_key: str = os.environ.get("OPENAI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+def get_embedding(text: str):
+    """Given a text returns its embedding"""
+    response = client.embeddings.create(
+        input=text,
+        model="text-embedding-3-small"
+    )
+    return response.data[0].embedding
