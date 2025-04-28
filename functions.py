65190af4be2fab16a0d6dc24f30373a75f7c01e8
Adrià Blancafort <adriablancafort@gmail.com>
2025-04-28 12:04:54 +0200

    add optimum time table function

--- functions.py
+++ functions.py
@@ -15,6 +15,20 @@ def activities_swipes(quiz_id: int):
 
 def activities_results(quiz_id: int):
     """Return the final personalized schedule results."""
-    ids = [1, 4, 10, 22]
+    
+    results = supabase.rpc(
+        "get_activities_by_user_preferences",
+        {
+            "input_quiz_id": quiz_id,
+        }
+    ).execute()
+
+    ids = optimum_timetable(results.data)
+
     results = supabase.table("activities").select("id,title,description,image_uri,start_time,end_time,schedules(title)").in_("id", ids).execute()
+    
     return results.data
+
+def optimum_timetable(input):
+    ids = [1, 4, 10, 22]
+    return ids
