942b53599aab07cce4feb2647a4a6bc62e060343
Adrià Blancafort <adriablancafort@gmail.com>
2025-04-29 16:38:08 +0200

    consider tags for activity embeding

--- scripts/generate_activities_embedings.py
+++ scripts/generate_activities_embedings.py
@@ -1,9 +1,9 @@
 from database import supabase
 from embedings import get_embedding
 
-results = supabase.table("activities").select("id,title,description,long_text").execute()
+results = supabase.table("activities").select("id,title,description,tags,long_text").execute()
 
 for activity in results.data:
-    activity_text = f"{activity['title']}. {activity['description']}. {activity['long_text']}"
+    activity_text = f"{activity['title']}. {activity['description']}. {activity['tags']} {activity['long_text']}"
     embedding = get_embedding(activity_text)
     supabase.table("activities").update({"embedding": embedding}).eq("id", activity["id"]).execute()
