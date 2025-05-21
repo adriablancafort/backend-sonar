16b2be5d84ae05bf648c699406e14c2189a60fd2
AniolGarrigaTorra <aniolaraos@gmail.com>
2025-05-21 20:23:20 +0200

    more things cris

--- download_pdf.py
+++ download_pdf.py
@@ -1,16 +1,16 @@
 import os
 from jinja2 import Environment, FileSystemLoader
-from weasyprint import HTML, CSS
+from weasyprint import HTML
 from main import read_results
+import time
 
-# 1) Configura Jinja2
 env = Environment(
-    loader=FileSystemLoader('template'),  # carpeta on estigui activities_template.html
+    loader=FileSystemLoader('template'),  
     autoescape=True
 )
-template = env.get_template('template_versio7.html')
+template = env.get_template('results_template.html')
 
-# Exemples de dades de prova per al nou template
+
 def get_day_dots(day):
     mapping = {
         "thursday": ["●","○","○"],
@@ -22,9 +22,16 @@ def get_day_dots(day):
 def get_time_icon(t):
     return "moon" if t.lower() == "night" else "sun"
 
+
+start=time.time()
+print("Starting generating schedule...")
+
 raw_activities = read_results(631)
 
+end1=time.time()
+print("Schedule generated in seconds: ", end1-start)
 
+print("Addind context to template...")
 # Construcció del context amb els camps calculats
 activities = []
 for act in raw_activities:
@@ -49,13 +56,20 @@ context = {
     "activities": activities
 }
 
+end2=time.time()
+print("Context added in seconds: ", end2-end1)
+print("Rendering template...")
 
 
-# 3) Renderitza l’HTML
 html_out = template.render(**context)
+end3=time.time()
+print("Template rendered in seconds: ", end3-end2)
+print("Generating PDF...")
 
 
 HTML(string=html_out, base_url=os.getcwd()) \
     .write_pdf("results_test.pdf")
-
-print("PDF generat: results_test.pdf")
+end4=time.time()
+print("PDF generated in seconds: ", end4-end3)
+print("Total time in seconds: ", end4-start)
+print("PDF generated successfully.")
